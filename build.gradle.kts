8662d66465f230d37360155245026594aaf5799a
Tommaso Pastorelli <tommaso.pastorelli1804@gmail.com>
2026-04-16 14:46:50 +0200

    3.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,10 @@ noArg {
 }
 
 group = "dev.tommasop1804"
-version = "3.7.0"
+version = "3.7.1"
 // Kotlin-Utils
 // Tommaso Pastorelli
-// Last update: Tommaso Pastorelli | 20260416T114557Z
+// Last update: Tommaso Pastorelli | 20260416T124633Z
 
 repositories {
     mavenCentral()
